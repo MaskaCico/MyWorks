a80fa0d96c1b4b4a228cdd9c332057ad9ac1c503
MaskaCico <13thcatto@gmail.com>
2025-07-22 21:47:41 +0200

    todo-complete

--- react-todo/src/ToDo.jsx
+++ react-todo/src/ToDo.jsx
@@ -2,26 +2,49 @@ import React from "react";
 import { useState } from "react";
 
 function ToDo() {
-  const [tasks, setTasks] = useState(["Egyes", "Kettes", "Hármas"]);
+  const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
 
   function handleInputChange(event) {
     setNewTask(event.target.value);
   }
 
-  function addTask() {}
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTasks((t) => [...t, newTask]);
+      setNewTask("");
+    }
+  }
 
-  function removeTask(index) {}
+  function removeTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  }
 
-  function moveTaskUp(index) {}
+  function moveTaskUp(index) {
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      const temp = updatedTasks[index - 1];
+      updatedTasks[index - 1] = updatedTasks[index];
+      updatedTasks[index] = temp;
+      setTasks(updatedTasks);
+    }
+  }
 
-  function moveTaskDown(index) {}
+  function moveTaskDown(index) {
+    if (index < tasks.length - 1) {
+      const updatedTasks = [...tasks];
+      const temp = updatedTasks[index + 1];
+      updatedTasks[index + 1] = updatedTasks[index];
+      updatedTasks[index] = temp;
+      setTasks(updatedTasks);
+    }
+  }
 
   return (
     <div className="todo-container">
       <h1>What to Do?</h1>
-
-      <div>
+      <div className="input-container">
         <input
           type="text"
           placeholder="Give me a task...."
@@ -44,14 +67,14 @@ function ToDo() {
               onClick={() => moveTaskUp(index)}
               disabled={index === 0}
             >
-              🔼
+              <span className="icon">🔼</span>
             </button>
             <button
               className="move-button"
               onClick={() => moveTaskDown(index)}
               disabled={index === tasks.length - 1}
             >
-              🔽
+              <span className="icon">🔽</span>
             </button>
           </li>
         ))}
